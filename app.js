0e2a20ec7169b4b383f05f5b8e1461c1d967f7b9
cnzau <clement.nzau@gmail.com>
2016-03-25 16:05:20 +0300

    added room offers, reduced onscroll value effect

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
       $(window).on('scroll', function() {
-        if (Math.round($(window).scrollTop()) > 100) {
+        if (Math.round($(window).scrollTop()) > 20) {
           $('.navbar').addClass('scrolled');
         } else {
           $('.navbar').removeClass('scrolled');
